7fe9495f4169a0169a832c3d15b7b4f2910f9390
hhshhhskk <hhshhhskk@naver.com>
2023-03-06 17:52:48 +0900

    관광 이미지 추가

--- src/api.js
+++ src/api.js
@@ -4,8 +4,8 @@ const API_KEY = "9%2Bmjut77JeCgLpJnD6eoWV7TctBLzXUqwzvjS5DZS4cXBdUNbCP7uYZFMMFwh
 
 const TYPE = "_type=json";
 
-export function getTourism() {
-    return fetch(`${BASE_PATH}/areaBasedList?numOfRows=12&pageNo=3&MobileOS=ETC&MobileApp=AppTest&ServiceKey=${API_KEY}&${TYPE}&listYN=Y&arrange=A&contentTypeId=12&areaCode=1`).then(
+export async function getTourism() {
+    return await fetch(`${BASE_PATH}/areaBasedList?numOfRows=12&pageNo=3&MobileOS=ETC&MobileApp=AppTest&ServiceKey=${API_KEY}&${TYPE}&listYN=Y&arrange=A&contentTypeId=12&areaCode=1`).then(
         (response) =>
             console.log(response.body)
     );
