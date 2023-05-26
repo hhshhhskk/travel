58f77cf2b38880fbe5f32b536a4b33ba6c3b352d
hhshhhskk <hhshhhskk@naver.com>
2023-05-26 18:38:59 +0900

    라우터추가

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+})
+
+app.use("/user", require("./routes/userRouter"));
+
+
+app.listen(8080, () => {
+    console.log("server start")
+})
